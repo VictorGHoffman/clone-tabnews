8f5980724dbcb19f6f135ab3d3a48daf562e870c
VictorGHoffman <victorghoff@gmail.com>
2023-08-11 21:53:08 -0300

    gera mensagem aleatoria

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
+import { useEffect, useState } from "react";
+let array = ["Bora bicho cade voce?", "acelera a mobilete", "o topo é seu, ta ligado?", "little chico?", "f no chat", "vacalos me mordam", "no vegans allowed", "ja acabou?"]
+
 function Home() {
-  return <h1>Bora bicho cade voce? </h1>;
+  const [mensagem, setMensagem] = useState("");
+  useEffect(() => {
+    const index = Math.floor(Math.random() * array.length);
+    setMensagem(array[index]);
+  })
+
+  return (
+    <div>
+      <h1>{mensagem}</h1>
+    </div>
+  );
 }
 
 export default Home;
